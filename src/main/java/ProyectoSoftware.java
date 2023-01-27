992990a32444eb1ea6ab6acc0db2dfdb999f0987
DrGrodus <ebermd00@gmail.com>
2023-01-27 18:39:09 -0500

    inicios de lectura de archivos

--- src/main/java/ProyectoSoftware.java
+++ src/main/java/ProyectoSoftware.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class ProyectoSoftware {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try {
+            File folder = new File("./CS13309_Archivos_HTML/Files/");
+            List<File> fileList = Arrays.asList(Objects.requireNonNull(folder.listFiles()));
+            for(File fileEntry : folder.listFiles()){
+                if(fileEntry.isDirectory()){
+                    list
+
+                }
+
+            }
+
+        } catch (NullPointerException e) {
+
+        }
     }
 }
