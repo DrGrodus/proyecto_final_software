2ada57835db3193511be62193d23fe48a7b6368f
DrGrodus <67439083+DrGrodus@users.noreply.github.com>
2023-03-07 19:28:51 -0500

    inicio con la actividad 6

--- src/main/java/com/example/demos/Actividad_6.java
+++ src/main/java/com/example/demos/Actividad_6.java
@@ -1,4 +1,25 @@
 package com.example.demos;
 
 public class Actividad_6 {
+
+    private long tiempoAct;
+    private TreeMap<File, Long> registros;
+
+    public long getTiempoAct() {
+        return tiempoAct;
+    }
+
+    public void setTiempoAct(long tiempoAct) {
+        this.tiempoAct = tiempoAct;
+    }
+
+    public TreeMap<File, Long> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(TreeMap<File, Long> registros) {
+        this.registros = registros;
+    }
+
+    
 }
